7fa83f1aa9a6727ad4a23fbee3bd605c0ece62fb
Rudi Strydom <iam@thatguy.co.za>
2015-09-17 16:25:11 +0200

    Documenting some more

--- render.js
+++ render.js
@@ -12,9 +12,12 @@ if (system.args.length <= 3) {
   phantom.exit(1);
 }
 
+// http://stackoverflow.com/a/31847721/2080324
 function hostname(url) {
   var match = url.match(/:\/\/(www[0-9]?\.)?(.[^/:]+)/i);
-  if ( match != null && match.length > 2 && typeof match[2] === 'string' && match[2].length > 0 ) return match[2];
+  if ( match != null && match.length > 2 && typeof match[2] === 'string' && match[2].length > 0 ) {
+    return match[2];
+  }
 }
 
 // Put it all together
